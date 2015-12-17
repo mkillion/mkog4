a5983a242bbd50557cf66b6d5a38a386d25043c2
Mike Killion <killion.kgs@gmail.com>
2015-12-17 10:25:03 -0600

    Add empty drawer.

--- js/main_mk.js
+++ js/main_mk.js
@@ -1,10 +1,28 @@
 require([
+	"dojo/_base/lang",
+	"dojo/on",
 	"application/Drawer",
-    "application/DrawerMenu",
+    "application/DrawerMenu"
 ],
 function(
+	lang,
+	on,
 	Drawer,
 	DrawerMenu
 ) {
-
+	this._drawer = new Drawer({
+        showDrawerSize: this._showDrawerSize,
+        borderContainer: 'bc_outer',
+        contentPaneCenter: 'cp_outer_center',
+        contentPaneSide: 'cp_outer_left',
+        toggleButton: 'hamburger_button'
+    });
+    // drawer resize event
+    on(this._drawer, 'resize', lang.hitch(this, function () {
+    	console.log("resize event");
+        // check mobile button status
+        //this._checkMobileGeocoderVisibility();
+    }));
+    // startup drawer
+    this._drawer.startup();
 } );
