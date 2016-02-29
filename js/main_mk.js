55fa89016a4f589929b41f15cbf6e34dd622ebea
Mike Killion <killion.kgs@gmail.com>
2016-02-29 12:21:14 -0600

    Add WWC5 filter (UI only).

--- js/main_mk.js
+++ js/main_mk.js
@@ -279,13 +279,13 @@ function(
     function createFilterDialogs() {
         // earthquakes:
         var magOptions = "<option value='all'>All</option><option value='2'>2.0 to 2.9</option><option value='3'>3.0 to 3.9</option><option value='4'>4.0 +</option>";
-        var eqFilter = "By Day<br>";
+        var eqFilter = "<span class='filter-hdr'>By Day:</span><br>";
         eqFilter += "<table><tr><td class='find-label'>From:</td><td><input type='text' size='10' id='eq-from-date' readonly></td></tr>";
         eqFilter += "<tr><td class='find-label'>To:</td><td><input type='text' size='10' id='eq-to-date' readonly></td></tr>";
         eqFilter += "<tr><td class='find-label'>Magnitude:</td><td><select name='day-mag' id='day-mag'>";
         eqFilter += magOptions;
         eqFilter += "</select></td></tr><tr><td></td><td><button class='find-button' id='day-btn' onclick='filterQuakes(this.id);'>Go</button></td></tr></table><hr>";
-        eqFilter += "By Year<br>";
+        eqFilter += "<span class='filter-hdr'>By Year</span><br>";
         eqFilter += "<table><tr><td class='find-label'>Year:</td><td><select name='year' id='year'><option value='all'>All</option>";
         for (var y=2016; y>2012; y--) {
             eqFilter += "<option value='" + y + "'>" + y + "</option>";
@@ -312,10 +312,26 @@ function(
         } );
         $("#eq-to-date").datepicker();
 
-        // TODO: wwc5 wells:
-        var wwc5Filter = "<table border='1'><tr><th>Use</th><th>Status</th><th>Completion Date</th></tr>";
+        // wwc5 wells:
+		var wwc5Status = ["Constructed","Plugged","Reconstructed"];
+		var wwc5Use = ["Air Conditioning","Cathodic Protection Borehole","Dewatering","Domestic","Domestic, Livestock","Domestic, changed from Irrigation","Domestic, changed from Oil Field Water Supply","Environmental Remediation, Air Sparge","Environmental Remediation, Injection","Environmental Remediation, Recovery","Environmental Remediation, Soil Vapor Extraction","Feedlot","Feedlot/Livestock/Windmill","Geothermal, Closed Loop, Horizontal","Geothermal, Closed Loop, Vertical","Geothermal, Open Loop, Inj. of Water","Geothermal, Open Loop, Surface Discharge","Heat Pump (Closed Loop/Disposal), Geothermal","Industrial","Injection well/air sparge (AS)/shallow","Irrigation","Lawn and Garden - domestic only","Monitoring well/observation/piezometer","Oil Field Water Supply","Other","Pond/Swimming Pool/Recreation","Public Water Supply","Recharge Well","Recovery/Soil Vapor Extraction/Soil Vent","Road Construction","Test Hole, Cased","Test Hole, Geotechnical","Test Hole, Uncased","Test hole/well","(unstated)/abandoned"];
+		var wwc5Filter = "<span class='filter-hdr'>Completion Date:</span><br>";
+        wwc5Filter += "<table><tr><td class='find-label'>From:</td><td><input type='text' size='10' id='wwc5-from-date' readonly></td>";
+        wwc5Filter += "<td class='find-label'>To:</td><td><input type='text' size='10' id='wwc5-to-date' readonly></td></tr></table>";
+		wwc5Filter += "<span class='filter-hdr'>Construction Status:</span><br><table>";
+		for (var i = 0; i < wwc5Status.length; i++) {
+			wwc5Filter += "<tr><td><input type='checkbox' name='const-status' value='" + wwc5Status[i] + "'>" + wwc5Status[i] + "</td></tr>"
+		}
+		wwc5Filter += "</table>"
+		wwc5Filter += "<span class='filter-hdr'>Well Use:</span><br><table>";
+		wwc5Filter += "<tr><td><select id='well-use' multiple><option value=''>-- select one or many (ctrl or cmd key) --</option -->";
+		for (var k = 0; k < wwc5Use.length; k++) {
+			wwc5Filter += "<option value='" + wwc5Use[k] + "'>" + wwc5Use[k] + "</option>";
 
-        wwc5Filter += "</table>";
+		}
+		wwc5Filter += "</select></td></tr>";
+		wwc5Filter += "<tr><td><button onclick='clearWwc5Filter();'>Clear Filter</button></td></tr>";
+		wwc5Filter += "<tr><td><button class='find-button' id='wwc5-go-btn' onclick='filterWWC5();' autofocus>Go</button></td></tr></table>";
 
         var wwc5N = domConstruct.create("div", { id: "wwc5-filter", class: "filter-dialog", title: "Filter Water Wells", innerHTML: wwc5Filter } );
         $("body").append(wwc5N);
@@ -323,12 +339,39 @@ function(
         $("#wwc5-filter").dialog( {
             autoOpen: false,
             dialogClass: "dialog",
-            width: 300
+            width: 450
         } );
+
+		$("#wwc5-from-date").datepicker();
+        $("#wwc5-to-date").datepicker();
 
         // TODO: og wells:
 
     }
+
+
+	filterWWC5 = function() {
+		// TODO: write query and def statement.
+		var conStatus = $('input[name="const-status"]:checked').map(function() {
+		    return this.value;
+		} ).get();
+		var wellUse = $("#well-use").val();
+		var wwc5FromDate = dom.byId("wwc5-from-date").value;
+		var wwc5ToDate = dom.byId("wwc5-to-date").value;
+
+		console.log(wwc5FromDate);
+		console.log(wwc5ToDate);
+		console.log(conStatus);
+		console.log(wellUse);
+	}
+
+
+	clearWwc5Filter = function() {
+		dom.byId("wwc5-from-date").value = "";
+        dom.byId("wwc5-to-date").value = "";
+		$('input[name="const-status"]').removeAttr("checked");
+		$('select#well-use option').removeAttr("selected");
+	}
 
 
     filterQuakes = function(btn) {
